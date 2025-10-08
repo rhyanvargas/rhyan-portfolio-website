944c1778b9920d51ded9b3597e5dc51127ed59a1
Rhyguy <rhyan@rhyanvargas.com>
2025-10-08 14:29:00 -0700

    keep coming soon up

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <Navigation />
-      <HeroSection />
+      {/* <HeroSection /> */}
       {/* <AboutSection /> */}
       {/* <ExperienceSection /> */}
       {/* <ProjectsSection /> */}
