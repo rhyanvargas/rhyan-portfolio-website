b04b551a128939991a5966f678d443b046916c9a
Rhyguy <rhyan@rhyanvargas.com>
2025-10-08 15:03:23 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ import { Footer } from "@/components/footer"
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Navigation />
+      {/* <Navigation /> */}
       {/* <HeroSection /> */}
       {/* <AboutSection /> */}
       {/* <ExperienceSection /> */}
